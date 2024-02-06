24a6a1f9b7f5e5f4c77f0a696f77d328af387307
Hugo <hugo.follmi@etu.univ-grenoble-alpes.fr>
2024-02-06 17:19:21 +0100

    Exo 3 mid

--- app/src/main/java/fr/iut2/androidtp/Exercice3Activity.java
+++ app/src/main/java/fr/iut2/androidtp/Exercice3Activity.java
@@ -1,10 +1,18 @@
 package fr.iut2.androidtp;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Exercice3Activity extends AppCompatActivity {
+
+    ImageView pcPaper, pcRock, pcSicor, myPaper, myRock, mySicor;
+    TextView result, nbWin, nbLose, nbNul;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +20,24 @@ public class Exercice3Activity extends AppCompatActivity {
 
         // On charge le XML pour créer l'arbre graphique
         setContentView(R.layout.activity_exercice3);
+
+        // PC
+        pcPaper = (ImageView) findViewById(R.id.pcPaper);
+        pcRock = (ImageView) findViewById(R.id.pcRock);
+        pcSicor = (ImageView) findViewById(R.id.pcSicor);
+
+        // PLayer
+        myPaper = (ImageView) findViewById(R.id.myPaper);
+        myRock = (ImageView) findViewById(R.id.myRock);
+        mySicor = (ImageView) findViewById(R.id.mySicor);
+
+        // text
+        result = (TextView) findViewById(R.id.result);
+        nbWin = (TextView) findViewById(R.id.nbWin);
+        nbLose = (TextView) findViewById(R.id.nbLose);
+        nbNul = (TextView) findViewById(R.id.nbNul);
+
+
     }
+
 }
